f242ae706bc294a2a43cf38c60db74a6933ffbe0
Yuda Yahya <yudayahya7@gmail.com>
2024-01-02 22:58:57 +0700

    add utility modules

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { camelize, getCurrentInstance, toHandlerKey } from 'vue'
+// import { camelize, getCurrentInstance, toHandlerKey } from 'vue'
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
