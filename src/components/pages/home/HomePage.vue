431624f1241a52e707b8fb3ba75e476a18cbc989
Yuda Yahya <yudayahya7@gmail.com>
2024-01-02 22:56:55 +0700

    create home page

---
+++ src/components/pages/home/HomePage.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Ini Home</div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { invoke } from '@tauri-apps/api'
+import HomePage from '@/components/pages/home/HomePage.vue'
+
+// now we can call our Command!
+// Right-click the application background and open the developer tools.
+// You will see "Hello, World!" printed in the console!
+invoke('greet', { name: 'World' })
+  // `invoke` returns a Promise
+  .then((response) => console.log(response))
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <HomePage />
 </template>
