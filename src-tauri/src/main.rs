4d1ec5ae7db5baefabf760aa3daae7589c44e302
Yuda Yahya <yudayahya7@gmail.com>
2024-01-07 20:30:01 +0700

    register user sign in command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
+mod commands;
+mod libs;
+mod repositories;
+
+#[tokio::main]
+async fn main() {
+    tauri::async_runtime::set(tokio::runtime::Handle::current());
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|_app| {
+            tokio::task::spawn(async move { libs::db::init() });
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![commands::user::sign_in])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
 }
