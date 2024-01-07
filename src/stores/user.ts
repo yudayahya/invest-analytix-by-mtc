22529d6035f985dcce15dcad5ccb08682c955531
Yuda Yahya <yudayahya7@gmail.com>
2024-01-08 04:38:52 +0700

    update user store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,17 +1,27 @@
 import { ref } from 'vue'
 import { defineStore, acceptHMRUpdate } from 'pinia'
 
+type UserType = {
+  email: string | null
+  created_at: string | null
+  logged_in_status: boolean
+}
+
 export const useUserStore = defineStore('user', () => {
-  const user = ref({
-    name: 'Yuda Yahya',
+  const user = ref<UserType>({
+    email: null,
+    created_at: null,
     logged_in_status: false
   })
 
-  const setLogin = () => {
+  const setLogin = (email: string | null, created_at?: string) => {
+    user.value.email = email
+    user.value.created_at = created_at || null
     user.value.logged_in_status = true
   }
 
   const setLogout = () => {
+    user.value.email = null
     user.value.logged_in_status = false
   }
 
