9c575e65cc7cdaf97c5b9ea65ae8bd24e878e621
Yuda Yahya <yudayahya7@gmail.com>
2024-01-08 04:35:59 +0700

    register toast

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Toaster from '@/components/ui/toast/ToasterComponent.vue'
+</script>
 
 <template>
   <RouterView />
+  <Toaster />
 </template>
