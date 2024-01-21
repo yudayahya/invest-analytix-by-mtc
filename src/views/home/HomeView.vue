80c18ecd63cccd2cd86de4e3e75615d26905ee14
Yuda Yahya <yudayahya7@gmail.com>
2024-01-22 01:41:02 +0700

    update home

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-import { invoke } from '@tauri-apps/api'
-
-type MyRes = {
-  data: string
-}
-
-// now we can call our Command!
-// Right-click the application background and open the developer tools.
-// You will see "Hello, World!" printed in the console!
-invoke<MyRes>('sign_in', { data: { email: 'yuda@amalan.org', password: '1234' } })
-  // `invoke` returns a Promise
-  .then((r) => console.log(r))
-  .catch((e) => console.log(e))
+import { useUserStore } from '@/stores/user'
+const userStore = useUserStore()
 </script>
 
 <template>
   <div>Ini Home</div>
+  <div>{{ userStore.user }}</div>
 </template>
