dff6862934b97538e6a2288547b43c50c53505d6
Yuda Yahya <yudayahya7@gmail.com>
2024-01-22 01:40:34 +0700

    set default class to dark

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import Toaster from '@/components/ui/toast/ToasterComponent.vue'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  document.querySelector('body')?.classList.add('dark')
+})
 </script>
 
 <template>
