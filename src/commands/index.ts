dfcc31b39f74d663e1396022e7905d0dac60ccf5
Yuda Yahya <yudayahya7@gmail.com>
2024-02-05 17:38:21 +0700

    create composable command invoke & available command list

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,4 +1,16 @@
-export type CommandResponse<T> = {
+import { invoke } from '@tauri-apps/api'
+
+type CommandResponse<T> = {
   message: string
   data: T
+}
+
+const CommandList = ['sign_in', 'create_account'] as const
+
+export const InvokeCommand = async <R>(command: (typeof CommandList)[number], data: object) => {
+  const result = await invoke<CommandResponse<R>>(command, {
+    data: { ...data }
+  })
+
+  return result
 }
