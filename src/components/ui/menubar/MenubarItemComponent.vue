e2b48ec6c49ed1fb7e8ba7235a7728d7befbbc9d
Yuda Yahya <yudayahya7@gmail.com>
2024-02-05 21:16:50 +0700

    make default mouse pointer

--- src/components/ui/menubar/MenubarItemComponent.vue
+++ src/components/ui/menubar/MenubarItemComponent.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
-import {
-  MenubarItem,
-  type MenubarItemEmits,
-  type MenubarItemProps,
-} from 'radix-vue'
+import { MenubarItem, type MenubarItemEmits, type MenubarItemProps } from 'radix-vue'
 import { cn } from '@/lib/utils'
 
 const props = defineProps<MenubarItemProps & { inset?: boolean; class?: string }>()
@@ -16,10 +12,10 @@ const emits = defineEmits<MenubarItemEmits>()
     v-bind="props"
     :class="[
       cn(
-        'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+        'relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
         inset && 'pl-8',
-        props.class,
-      ),
+        props.class
+      )
     ]"
     @select="emits('select', $event)"
   >
--- src/components/ui/menubar/MenubarTriggerComponent.vue
+++ src/components/ui/menubar/MenubarTriggerComponent.vue
@@ -10,8 +10,8 @@ const props = defineProps<MenubarTriggerProps & { class?: string }>()
     v-bind="props"
     :class="
       cn(
-        'flex cursor-default select-none items-center rounded-sm px-3 py-1.5 text-sm font-medium outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground',
-        props.class,
+        'flex cursor-pointer select-none items-center rounded-sm px-3 py-1.5 text-sm font-medium outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground',
+        props.class
       )
     "
   >
