3e2592a34a07e8e3c531ff8752c3011af94ae203
Yuda Yahya <yudayahya7@gmail.com>
2024-02-05 21:15:55 +0700

    provide page title component

---
+++ src/components/ui/title/PageTitleComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<template>
+  <div class="w-full mt-4 pb-4 border-b">
+    <h2 class="text-2xl font-bold tracking-tight">Halaman {{ route.meta.title ?? '-' }}</h2>
+    <p class="text-muted-foreground">{{ route.meta.subtitle ?? '-' }}</p>
+  </div>
+</template>
