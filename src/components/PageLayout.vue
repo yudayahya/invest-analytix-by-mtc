2ce694db64e7ac68fbf1e784fff49bfdc978c28f
Yuda Yahya <yudayahya7@gmail.com>
2024-02-05 21:16:20 +0700

    adjust & register breadcrumb and page title

---
+++ src/components/PageLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import MenuBar from './MenuBar.vue'
+import { BreadCrumb } from './ui/breadcrumb'
+import { PageTitle } from './ui/title'
+</script>
+
+<template>
+  <div class="w-full">
+    <MenuBar />
+    <div class="w-full min-h-screen pt-12 px-4">
+      <BreadCrumb />
+      <PageTitle />
+      <RouterView />
+    </div>
+  </div>
+</template>
