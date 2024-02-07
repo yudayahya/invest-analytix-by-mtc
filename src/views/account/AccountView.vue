03bc93da273b610092ae2a22e65c1045e8291f2b
Yuda Yahya <yudayahya7@gmail.com>
2024-02-08 04:58:27 +0700

    provide account view page

---
+++ src/views/account/AccountView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useAccountStore } from '@/stores/account'
+import { onMounted } from 'vue'
+
+const accountStore = useAccountStore()
+
+onMounted(() => {
+  accountStore.fetch_accounts()
+})
+</script>
+
+<template>
+  <div>
+    <h1>Account</h1>
+  </div>
+  <div>{{ accountStore.accounts }}</div>
+</template>
