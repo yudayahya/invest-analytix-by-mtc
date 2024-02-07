29f2fc2285302d51a0ed35d77a13a52833bd0949
Yuda Yahya <yudayahya7@gmail.com>
2024-02-08 05:00:55 +0700

    register more command & spawn webview console on startup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 mod commands;
 mod libs;
 mod repositories;
@@ -10,18 +12,23 @@ async fn main() {
     tauri::async_runtime::set(tokio::runtime::Handle::current());
 
     tauri::Builder::default()
-        .setup(|_app| {
+        .setup(|app| {
             tokio::task::spawn(async move {
                 libs::db::init()
                     .await
                     .expect("failed while initializing database");
             });
 
+            let window = app.get_window("main").unwrap();
+            window.open_devtools();
+
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
             commands::user::sign_in,
-            commands::account::create_account
+            commands::account::get_account,
+            commands::account::get_ib_account,
+            commands::account::create_account,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
