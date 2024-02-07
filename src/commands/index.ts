334faa1ab6d960085eef52ffc7945fe0336b8172
Yuda Yahya <yudayahya7@gmail.com>
2024-02-08 04:56:04 +0700

    add more command list & set zod error map

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,15 +1,24 @@
 import { invoke } from '@tauri-apps/api'
+import { z } from 'zod'
+
+z.setErrorMap((issue, ctx) => {
+  if (issue.code === z.ZodIssueCode.invalid_type) {
+    return { message: 'Mohon diisi' }
+  }
+
+  return { message: ctx.defaultError }
+})
 
 type CommandResponse<T> = {
   message: string
   data: T
 }
 
-const CommandList = ['sign_in', 'create_account'] as const
+const CommandList = ['sign_in', 'create_account', 'get_account', 'get_ib_account'] as const
 
-export const InvokeCommand = async <R>(command: (typeof CommandList)[number], data: object) => {
+export const InvokeCommand = async <R>(command: (typeof CommandList)[number], data?: object) => {
   const result = await invoke<CommandResponse<R>>(command, {
-    data: { ...data }
+    data: data ? { ...data } : undefined
   })
 
   return result
