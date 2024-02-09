81f29264f46e854e8e77e791f60ae82d5dc3d0ea
Yuda Yahya <yudayahya7@gmail.com>
2024-02-10 04:27:00 +0700

    create account view page

--- src/views/account/AccountView.vue
+++ src/views/account/AccountView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { useAccountStore } from '@/stores/account'
 import { onMounted } from 'vue'
+import { createAccountColumnDefinitions, accountColumnStruct } from './column-definition'
+import { DataTable } from '@/components/datatable'
 
 const accountStore = useAccountStore()
+const accountColumnDefinitions = createAccountColumnDefinitions()
 
 onMounted(() => {
   accountStore.fetch_accounts()
@@ -10,8 +13,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <h1>Account</h1>
+  <div class="container py-8 mx-auto">
+    <DataTable
+      :columns="accountColumnDefinitions"
+      :data="accountStore.accounts"
+      :column-struct="accountColumnStruct"
+    />
   </div>
-  <div>{{ accountStore.accounts }}</div>
 </template>
